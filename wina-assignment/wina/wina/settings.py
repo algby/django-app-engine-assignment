59f4b9884f0dc94ad01813b5bd4b2e20068b3a52
James White <dev.jameswhite@gmail.com>
2013-10-04 16:36:19 +0100

    For compatibility with GAE

--- wina-assignment/wina/wina/settings.py
+++ wina-assignment/wina/wina/settings.py
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'wina.urls'
+ROOT_URLCONF = 'wina'
 
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'wina.wsgi.application'
