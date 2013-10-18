c10ea5e17287b4d12d7bcfda65e4cddf58440613
James White <james.white.15@gmail.com>
2013-10-18 15:20:33 +0100

    Removing trailing slashes from urls and adding some routes for media

--- cms/urls.py
+++ cms/urls.py
@@ -4,17 +4,19 @@ from cms import views
 
 urlpatterns = patterns('',
     # Authentication Routes
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'cms/auth/login.html'}, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^/login$', 'django.contrib.auth.views.login', {'template_name': 'cms/auth/login.html'}, name='login'),
+    url(r'^/logout$', views.logout, name='logout'),
 
     # Media Routes
-    url(r'^media/$', views.media, name='media-home'),
+    url(r'^/media$', views.media, name='media-home'),
+    url(r'^/media/add$', views.media_add, name='media-add'),
+    url(r'^/media/view/(?P<id>\d)$', views.media_edit, name='media-edit'),
 
     # Story Routes
-    url(r'^story/$', views.story, name='story-home'),
+    url(r'^/story$', views.story, name='story-home'),
 
     # User Routes
-    url(r'^user/$', views.user, name='user-home'),
+    url(r'^/user$', views.user, name='user-home'),
 
     # Catch all route
     url(r'^$', views.index, name='cms-home')
--- wina/urls.py
+++ wina/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Include the routes for the api module
-    url(r'^api/', include('api.urls')),
+    url(r'^api', include('api.urls')),
 
     # Include the routes for the cms module
-    url(r'^cms/', include('cms.urls')),
+    url(r'^cms', include('cms.urls')),
 
     # Include the routes for the site frontend
     url(r'^$', include('frontend.urls')),
