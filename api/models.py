ee1459622ee8b9f10e390d8c278e7fdb510a27b4
James White <james.white.15@gmail.com>
2013-10-25 01:41:44 +0100

    Temp fix for uploads and adding a to string method

--- api/models.py
+++ api/models.py
@@ -14,7 +14,10 @@ class Media(models.Model):
     type = models.CharField(max_length=5, choices=MEDIA_TYPES)
     content = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
-    file = models.FileField()
+    file = models.FileField(upload_to='/')
+
+    def __unicode__(self):
+	return self.title
 
 # Used to convert the media model to a form in the cms
 class MediaForm(forms.ModelForm):
