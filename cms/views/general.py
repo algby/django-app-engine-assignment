39b1bdb70e6f3653420cdf0024b3ab0c714e14f6
James White <james.white.15@gmail.com>
2013-10-27 20:33:46 +0000

    Tidying

--- cms/views/general.py
+++ cms/views/general.py
@@ -5,7 +5,7 @@ from django.contrib.auth import logout as logout_user
 # Render the CMS home page if the user is logged in
 @login_required
 def index(request):
-    return render(request, 'cms/index.html', {'title': 'Welcome, %s' % request.user})
+    return render(request, 'cms/index.html', {'title': 'Welcome, %s' % request.user.first_name})
 
 # Log out the user and redirect to them to /
 def logout(request):
