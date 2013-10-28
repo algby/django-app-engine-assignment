a9fafc82e4c52cbedb4bdbb4f6e10733c60a5d11
James White <james.white.15@gmail.com>
2013-10-28 19:50:34 +0000

    Starting on general search

--- cms/views/general.py
+++ cms/views/general.py
@@ -1,6 +1,12 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout as logout_user
+from django.core import serializers
+from django.http import HttpResponse
+
+from itertools import chain
+
+from api.models import Media, Story
 
 # Render the CMS home page if the user is logged in
 @login_required
@@ -14,3 +20,16 @@ def logout(request):
 
     # Redirect them to the home page
     return redirect('/')
+
+# Handles searching Media and Stories and returning as JSON
+def search(request, query):
+    # Search for the Media objects
+    media = Media.objects.filter(title__icontains=query)
+
+    # Search for the Story objects
+    stories = Story.objects.filter(title__icontains=query)
+
+    # Serialize the data as json
+    data = serializers.serialize('json', list(chain(media, stories)))
+
+    return HttpResponse(data, mimetype='application/json')
