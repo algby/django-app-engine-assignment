b81e6550d48620e371e4288793aa11a353c76d2b
James White <james.white.15@gmail.com>
2013-10-29 21:51:57 +0000

    Integrating permissions into users

--- cms/views/user.py
+++ cms/views/user.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib import messages
 from django.contrib.auth.models import Group
+from django.core.exceptions import PermissionDenied
 
 from api.models import CustomUser, CustomUserForm
 
@@ -12,20 +13,34 @@ def user(request):
 
     return render(request, 'cms/user/index.html', {'title': 'Users', 'users': users})
 
-# Render the add/edit media form or handle saving it
+# Render the add/edit user form or handle saving it
 @login_required
 def user_add_or_edit(request, id=False):
+    current_user = request.user
+
     # Is it a POST request, i.e is the form being submitted
     if request.method == 'POST':
         # If the id is not false then we are editing, so we need to
         # get an instance of that user first
         if id is not False:
             user = CustomUser.objects.get(id=id)
-            user_form = CustomUserForm(request.POST, instance=user)
 
-        # Otherwise we can just pass the form data straight to the form
+            # Check the has valid permissions to edit this user
+            if current_user.has_perm('auth.wina_edit_any_user') or (current_user.id == user.id and current_user.has_perm('auth.wina_edit_own_user')):
+                user_form = CustomUserForm(request.POST, instance=user)
+
+            else:
+                raise PermissionDenied
+
+        # Otherwise we can just pass the form data straight to the form, this is
+        # a POST request for adding user
         else:
-            user_form = CustomUserForm(request.POST)
+            # Check the user has valid permissions to add user
+            if current_user.has_perm('auth.wina_add_user'):
+                user_form = CustomUserForm(request.POST)
+
+            else:
+                raise PermissionDenied
 
         # Run through any validation rules we have
         if user_form.is_valid():
@@ -54,13 +69,24 @@ def user_add_or_edit(request, id=False):
     # Were we passed the id? i.e are we editing an object, if so get it to pass to the template
     if id is not False:
         user = CustomUser.objects.get(id=id)
-        user_form = user_form if request.method == 'POST' else CustomUserForm(instance=user)
-        template_data = {'form': user_form, 'title': user.first_name + ' ' + user.last_name}
+
+        # Check the user has valid permissions to edit this user
+        if current_user.has_perm('auth.wina_edit_any_user') or (current_user.id == user.id and current_user.has_perm('auth.wina_edit_own_user')):
+            user_form = user_form if request.method == 'POST' else CustomUserForm(instance=user)
+            template_data = {'form': user_form, 'title': user.first_name + ' ' + user.last_name}
+
+        else:
+            raise PermissionDenied
 
     # If not we must be adding new user as we have no id in the URL
     else:
-        user_form = user_form if request.method == 'POST' else CustomUserForm()
-        template_data = {'form': user_form, 'title': 'Add User'}
+        # Check the user has valid permissions to add a user
+        if current_user.has_perm('auth.wina_add_user'):
+            user_form = user_form if request.method == 'POST' else CustomUserForm()
+            template_data = {'form': user_form, 'title': 'Add User'}
+
+        else:
+            raise PermissionDenied
 
     return render(request, 'cms/form.html', template_data)
 
@@ -74,31 +100,43 @@ def user_view(request, id):
 # Handles activating another user if the current user is logged in
 @login_required
 def user_activate(request, id):
-    # Look up the user object
+    # Look up the user object and get the current user
     user = CustomUser.objects.get(id=id)
+    current_user = request.user
 
-    # Deactive the user
-    user.is_active = True
-    user.save()
+    # Check the user has valid permissions to delete this story
+    if current_user.has_perm('auth.wina_activate_any_user'):
+        # Activate the user
+        user.is_active = True
+        user.save()
 
-    # Show a success message to the user
-    messages.success(request, 'User succesfully activated!')
+        # Show a success message to the user
+        messages.success(request, 'User succesfully activated!')
 
-    # Redirect them back to the user home page
-    return redirect('user-home')
+        # Redirect them back to the user home page
+        return redirect('user-home')
+
+    else:
+        raise PermissionDenied
 
 # Handles deactivating another user if the current user is logged in
 @login_required
 def user_deactivate(request, id):
-    # Look up the user object
+    # Look up the user object and get the current user
     user = CustomUser.objects.get(id=id)
+    current_user = request.user
 
-    # Deactive the user
-    user.is_active = False
-    user.save()
+    # Check the user has valid permissions to delete this story
+    if current_user.has_perm('auth.wina_activate_any_user'):
+        # Deactive the user
+        user.is_active = False
+        user.save()
 
-    # Show a success message to the user
-    messages.success(request, 'User succesfully deactivated!')
+        # Show a success message to the user
+        messages.success(request, 'User succesfully deactivated!')
 
-    # Redirect them back to the user home page
-    return redirect('user-home')
+        # Redirect them back to the user home page
+        return redirect('user-home')
+
+    else:
+        raise PermissionDenied
