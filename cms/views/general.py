505ea98eec31ceec5a733282f4aa54b8d2c24465
James White <james.white.15@gmail.com>
2013-11-28 19:05:56 +0000

    Adding widget to cmd home page

--- cms/views/general.py
+++ cms/views/general.py
@@ -6,12 +6,24 @@ from django.http import HttpResponse
 
 from itertools import chain
 
-from api.models import Media, Story
+from api.models import Media, Story, CustomGroup, CustomUser
 
 # Render the CMS home page if the user is logged in
 @login_required
 def index(request):
-    return render(request, 'cms/index.html', {'title': 'Welcome, %s' % request.user.first_name})
+    # Get counts of all the content types
+    media_count = Media.objects.count()
+    story_count = Story.objects.count()
+    user_count = CustomUser.objects.count()
+    group_count = CustomGroup.objects.count()
+
+    return render(request, 'cms/index.html', {
+        'title': 'Welcome, %s' % request.user.first_name,
+        'media_count': media_count,
+        'story_count': story_count,
+        'user_count': user_count,
+        'group_count': group_count,
+    })
 
 # Log out the user and redirect to them to /
 def logout(request):
