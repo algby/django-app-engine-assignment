0571a299669c51e09ffbe53750bec7494d5a485a
James White <james.white.15@gmail.com>
2013-11-28 19:40:53 +0000

    Basic REST GET api for media

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, url
 from api import views
 
 urlpatterns = patterns('',
+	# Media Routes
+    url(r'^/media$', views.media, name='api-media'),
+
     # Catch all route
     url(r'^$', views.index, name = 'index')
 )
--- api/views.py
+++ api/views.py
@@ -1,5 +1,39 @@
 from django.http import HttpResponse
+from django.utils.dateformat import format
 
-# Create your views here.
+import json
+
+from api.models import Media
+
 def index(request):
-    return HttpResponse('api')
+    return HttpResponse(json.dumps({'message': 'Welcome to the WINA API!'}), mimetype='application/json')
+
+# Return all media as json
+def media(request):
+    # Get all the media objects
+    media = Media.objects.all()
+
+    # Set up a dict to return
+    response = {
+        'message': None,
+        'data': [],
+    }
+
+    # Append all the media objects to the response object
+    for object in media:
+        response['data'].append({
+            'id': object.id,
+            'title': object.title,
+            'type': object.type,
+            'content': object.content,
+            'author': {
+                'id': object.author_id,
+                'username': object.author.username,
+                'first_name': object.author.first_name,
+                'last_name': object.author.last_name,
+            },
+            'date_created': int(format(object.date_created, 'U')),
+        })
+
+    # Return the response as json
+    return HttpResponse(json.dumps(response), mimetype='application/json')
