3c09896cc965b7fb33e886a131654a7ff4d211d1
James White <james.white.15@gmail.com>
2013-12-03 00:14:06 +0000

    Add new search

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, url
-
 from api import views
 
 urlpatterns = patterns('',
     # Media Routes
     url(r'^/media$', views.media, name='api-media'),
     url(r'^/media/(?P<id>\d+)$', views.media_lookup, name='api-media-lookup'),
+
+    # Search routes
+    url(r'^/search$', views.search, name='api-search'),
 
     # Catch all route
     url(r'^$', views.index, name = 'index')
--- api/views.py
+++ api/views.py
@@ -1,26 +1,27 @@
 from django.http import HttpResponse
 from django.utils.dateformat import format
-
-import json
-
+from google.appengine.api import search as gsearch
 from api.models import Media
+import json
 
 def index(request):
     return HttpResponse(json.dumps({'message': 'Welcome to the WINA API!'}), mimetype='application/json')
 
-def __formatMedia(object):
+# Slightly messy method that abstracts away the differences between search and db objects
+def __formatMediaOrStory(object, type='db_object'):
     return {
-        'id': object.id,
-        'title': object.title,
-        'type': object.type,
-        'content': object.content,
+        'id': object['id'] if type == 'search_object' else object.id,
+        'title': object['title'] if type == 'search_object' else object.title,
+        'doc_type': object['doc_type'] if type == 'search_object' else object.doc_type,
+        'type': object['type'] if type == 'search_object' else object.type,
+        'content': object['content'] if type == 'search_object' else object.content,
         'author': {
-            'id': object.author_id,
-            'username': object.author.username,
-            'first_name': object.author.first_name,
-            'last_name': object.author.last_name,
+            'id': object['author_id'] if type == 'search_object' else object.author_id,
+            'username': object['author_username'] if type == 'search_object' else object.author.username,
+            'first_name': object['author_first_name'] if type == 'search_object' else object.author.first_name,
+            'last_name': object['author_last_name'] if type == 'search_object' else object.author.last_name,
         },
-        'date_created': int(format(object.date_created, 'U')),
+        'date_created': int(format(object['date_created'] if type == 'search_object' else object.date_created, 'U')),
     }
 
 # Return all media as json
@@ -53,4 +54,43 @@ def media_lookup(request, id):
     }
 
     # Return the response as json
+    return HttpResponse(json.dumps(response), mimetype='application/json')
+
+# Search for a story
+def search(request):
+    # Get the query and type
+    query = request.GET.get('query', None)
+    doc_type = request.GET.get('doc_type', None)
+
+    # Was a query passed?
+    if query:
+        # Set the index to use
+        index = gsearch.Index(name='stories-and-media')
+
+        # Append a type filter if it was specified
+        if doc_type:
+            query += ' doc_type = %s' % doc_type
+
+        # Run the search and set up the default response object
+        results = index.search(query)
+        response = {
+            'error': None,
+            'data': []
+        }
+
+        # Format the results in the same way as the rest of the API methods
+        for search_item in results:
+            item = {'id': int(search_item.doc_id.split(':')[1])}
+
+            for field in search_item.fields:
+                item[field.name] = field.value
+
+            response['data'].append(__formatMediaOrStory(item, type='search_object'))
+
+    else:
+        response = {
+            'error': 'Please pass a query to make a search!',
+            'data': []
+        }
+
     return HttpResponse(json.dumps(response), mimetype='application/json')
