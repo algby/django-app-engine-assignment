935a279c1ed85ae15ade7142af0252368e32a219
James White <james.white.15@gmail.com>
2013-12-09 03:36:04 +0000

    Improvements to voting

--- static/frontend/js/main.js
+++ static/frontend/js/main.js
@@ -4,29 +4,43 @@ $(document).ready(function() {
 	$('.upvote, .downvote').on('click', function() {
 
 		var $this = $(this);
-		var type = $this.data('type');
-		var story_id = $this.parents('.frontend-story').data('story-id');
 
-		// Submit the ajax request to increment or decrement the vote count
-		$.ajax({
-			url: '/vote?type=' + type + '&story_id=' + story_id,
-			cache: false
-		}).done(function(result) {
+		// Check voting hasn't been 'disabled' on this entry
+		if ($this.parent().data('voted') !== 'true') {
 
-			// Was there an error?
-			if (result.error !== null) {
+			var type = $this.data('type');
+			var story_id = $this.parents('.frontend-story').data('story-id');
+			var opposite = type === 'upvote' ? type : 'downvote';
 
-				alert('There was an error recording your vote, please try again');
-				console.log(result);
+			// Submit the ajax request to increment or decrement the vote count
+			$.ajax({
+				url: '/vote?type=' + type + '&story_id=' + story_id,
+				cache: false
+			}).done(function(result) {
 
-			// If not we can update the vote count!
-			} else {
+				// Was there an error?
+				if (result.error !== null) {
 
-				$('#' + story_id + '-votes').text(result.data.total);
+					alert('There was an error recording your vote, please try again');
+					console.log(result);
 
-			}
+				// If not we can update the vote count!
+				} else {
 
-		});
+					// Update the vote count
+					$('#' + story_id + '-votes').text(result.data.total);
+
+					// Change the colour of the other arrow
+					$this.parent().find('> .' + opposite).css('color', '#aaa');
+
+					// 'disable' further voting
+					$this.parent().data('voted', 'true');
+
+				}
+
+			});
+
+		}
 
 	});
 
